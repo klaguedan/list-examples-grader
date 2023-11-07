139027c82b281e0b7cb0792b118d4ac3757856f1
klaguedan <146864422+klaguedan@users.noreply.github.com>
2023-11-07 09:09:12 -0800

    updated grade.sh

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -14,3 +14,22 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+if [[ -f student-submission/ListExamples.java ]]
+then
+    # Move everything into grading-area/
+    studentcode="student-submission/listExamples.java"
+    test="grading-area/TestListExamples.java"
+    echo $?
+
+    cp -r $studentcode TestListExamples.java grading-area/
+    ls grading-area/
+
+    # Start Testing
+    javac -cp $CPATH grading-area/*.java
+    java -cp $CPATH org.junit.runner.JUnitCore $test 
+
+    ls grading-area/
+else
+    echo "Error! Correct file not recieved!"
+fi
